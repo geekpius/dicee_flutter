1bc9f8d6abec7d10d55533b311dd7fb7ad440e5d
Fiifi Pius Geek <fiifipius@gmail.com>
2020-06-05 20:02:24 +0000

    replace actions with a function

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,13 @@ class _DicePageState extends State<DicePage> {
   var leftDiceNumber = 1;
   var rightDiceNumber = 1;
 
+  void playDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -34,9 +41,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
+                playDice();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -44,9 +49,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
+                playDice();
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
